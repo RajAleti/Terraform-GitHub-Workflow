a19f3db8ee8db5d7f739d1451ad6a2eecb30f708
Raja <rajaleti111.origin@gmail.com>
2022-06-30 16:05:59 -0500

    changes made to yaml file

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,15 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "latest"
     }
   }
- backend "azurerm" {
-    resource_group_name  = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-  }
+ 
 }
 
 # Configure the Microsoft Azure Provider

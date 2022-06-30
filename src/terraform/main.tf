4bd2797ef5cbcd852e014f28a863816a410e0a0a
Raja <rajaleti111.origin@gmail.com>
2022-06-30 16:08:17 -0500

    changes made to yaml file

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,19 +1,19 @@
-terraform {
+Terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "latest"
+      version = "=3.0.0"
     }
   }
- 
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
+
 # Create a resource group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "east us"
+  location = "West Europe"
 }
